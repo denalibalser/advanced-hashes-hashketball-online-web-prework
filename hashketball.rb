a2c54d0218eef035a284cb5348fc49d3f3b49b2f
Denali Balser <denali.balser167@gmail.com>
2019-05-07 19:08:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -187,13 +187,13 @@ end
 def player_stats(player_name)
   stats_hash = {}
   game_hash.each do |location, attributes|
-
+binding.pry
     attributes[:players].each do |stats|
 
       if stats[:player_name] == player_name
 
         stats_hash[stats]
-        binding.pry
+        
 
     end
   end
